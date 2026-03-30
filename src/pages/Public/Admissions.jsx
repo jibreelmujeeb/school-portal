9327efa7d6b145bab2449e15f14bf175a472b28e
jibreel mujeeb <jibreelmujeeb@gmail.com>
2026-03-30 10:58:21 +0100

    update on admission page

--- src/pages/Public/Admissions.jsx
+++ src/pages/Public/Admissions.jsx
@@ -118,7 +118,9 @@ const Admissions = () => {
           {/* CLASS */}
           <select className="w-full border border-gray-300 rounded-lg px-4 py-2 text-sm outline-none">
             <option>Select Class</option>
-            <option>Primary</option>
+            <option>Creche</option>
+             <option>nursery</option>
+              <option>Primary</option>
             <option>Secondary</option>
           </select>
 
